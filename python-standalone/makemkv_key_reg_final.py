db6677e9a91b90e6c704d5b50f4b43e5e63b9e65
MiTechMess <108775098+MiTechMess@users.noreply.github.com>
2023-04-16 15:09:09 +0100

    Update makemkv_key_reg_final.py
    script worked but for easy human reading of the code i move the print output to happen after adding to the registry.

--- python-standalone/makemkv_key_reg_final.py
+++ python-standalone/makemkv_key_reg_final.py
@@ -13,15 +13,15 @@ soup = BeautifulSoup(html, 'html.parser')
 # Find the first <code> block and extract its contents as the key value
 key_value = soup.find('code').text
 
-# Print a message to the console
-print('##################################')
-print('The key has been added to MakeMKV')
-print('##################################')
-
 # Update the registry with the key value
 key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, r'Software\MakeMKV')
 winreg.SetValueEx(key, 'app_Key', 0, winreg.REG_SZ, key_value)
 winreg.CloseKey(key)
+
+# Print a message to the console
+print('##################################')
+print('The key has been added to MakeMKV')
+print('##################################')
     
 # Wait for the user to press Enter before exiting
 input('\nPress Enter to exit...')
